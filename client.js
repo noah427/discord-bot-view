0c903332695e02fe41a1726d2bc8caf550c6f902
noah feinberg <redacted427@gmail.com>
2019-07-01 19:26:30 -0400

    first

---
+++ client.js
@@ -0,0 +1,31 @@
+var discord = require('discord.js')
+var client = new discord.Client
+var exports = module.exports = {}
+
+client.on('ready', () => console.log("logged in as: " + client.user.tag))
+
+exports.login = function (token) {
+    client.login(token)
+}
+
+exports.init = function () {
+    let guilds = []
+    client.guilds.forEach(guild => guilds.push(guild.name))
+    return guilds
+}
+
+exports.fetchChannels = function (serverName) {
+    let channels = []
+    client.guilds.find(server => server.name === serverName).channels.forEach(channel => {
+        if (channel.type === "text") {
+            channels.push(channel.name)
+        }
+    })
+    return channels
+}
+
+exports.fetchMsgs = async function (serverName, channelName) {
+    fetched = await client.guilds.find(guild => guild.name === serverName).channels.find(channel => channel.name === channelName).fetchMessages({ limit: 10 })
+    return fetched
+}
+
---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express')
+var app = express()
+var bot = require('./client')
+var io = require('socket.io')(8080);
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/public/index.html")
+})
+
+app.get('/login', function (req, res) {
+    bot.login(req.query.token)
+    res.sendFile(__dirname + "/public/interface.html")
+})
+
+
+io.on('connection', function (socket) {
+    console.log("connected")
+
+    var guilds = bot.init()
+
+
+    socket.emit('guilds', guilds)
+    socket.on('serverChannels', (serverName, serverNumber) => {
+        var channels = bot.fetchChannels(serverName)
+        socket.emit('sendChannels', channels, serverNumber)
+    });
+    socket.on('fetchChannelMessages', function (serverNum, channelName) {
+        var fetched = bot.fetchMsgs("testy", channelName)
+        var displayed = []
+        var msgCount = 0
+        fetched.then(function (messages) {
+            messages.forEach(function (message) {
+                msgCount++
+                displayed.push(`${message.author.tag}: Said : ${message.content}`)
+                if (msgCount === 10) {
+                    displayed.reverse()
+                    socket.emit('displayMessages', displayed)
+                }
+            });
+        })
+    })
+})
+
+
+app.listen('3000', function () {
+    console.log('listning on localhost:3000')
+})
