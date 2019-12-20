b26212eb00525a5f4f742f31f4105ac087f846c8
DEMONITIZED BOI <42183865+dexterateed@users.noreply.github.com>
2019-12-20 09:50:20 -0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,1 +1,5 @@
 npm install
+@echo off
+echo If you require help installing FFMPEG for the sound effects, please visit this link:
+echo http://blog.gregzaal.com/how-to-install-ffmpeg-on-windows/
+pause
